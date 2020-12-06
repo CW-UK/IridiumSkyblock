350dd7568588176d9563250b922d042d3471b44f
Craig <bleepy@gmail.com>
2020-12-06 12:43:54 +0000

    Revision

--- src/main/java/com/iridium/iridiumskyblock/listeners/CreatureSpawnListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/CreatureSpawnListener.java
@@ -16,11 +16,12 @@ public class CreatureSpawnListener implements Listener {
     public void onCreatureSpawnEvent(CreatureSpawnEvent event) {
         try {
             final Config config = IridiumSkyblock.getConfiguration();
+            if (!config.disableNaturalAnimalSpawns && !config.disableNaturalMonsterSpawns) return;
+
             final Entity entity = event.getEntity();
             final Location location = entity.getLocation();
-            if (!IridiumSkyblock.getIslandManager().isIslandWorld(location) || event.getSpawnReason() != CreatureSpawnEvent.SpawnReason.NATURAL || (!config.disableNaturalAnimalSpawns && !config.disableNaturalMonsterSpawns)) {
-                return;
-            }
+            if (!IridiumSkyblock.getIslandManager().isIslandWorld(location) || event.getSpawnReason() != CreatureSpawnEvent.SpawnReason.NATURAL) return;
+
             if ((entity instanceof Monster && config.disableNaturalMonsterSpawns) || (entity instanceof Animals && config.disableNaturalAnimalSpawns)) {
                 event.setCancelled(true);
                 return;
